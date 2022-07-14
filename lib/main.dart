eb1a573d9ab5e148026743933e1b11a20982ea55
tereyali22 <82040336+Linlin21@users.noreply.github.com>
2022-07-14 14:58:46 +0300

    commit deneme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp( MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'miCard',
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+        body: Container(
+          color: Colors.white,
+          child: Text('Hello'),
+        ),
+      )
+    );
+  }
 }
+
