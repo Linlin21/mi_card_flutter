1837ebe8c9acdab9869ada2f0b03038644454225
tereyali22 <82040336+Linlin21@users.noreply.github.com>
2022-07-14 15:02:16 +0300

    commit deneme 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'miiiCard',
+      title: 'miii6Card',
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(
